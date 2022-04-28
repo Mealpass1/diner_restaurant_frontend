b796c34ba9c17c7c66881378602e2dc78b79fbc0
Saad <byiringirosaad@gmail.com>
2022-04-28 21:13:11 +0200

    Update payment.jsx

--- src/diner/cart/payment.jsx
+++ src/diner/cart/payment.jsx
@@ -2,15 +2,28 @@ import React from "react";
 import styled from "styled-components";
 
 import Layout from "../../components/diner/Layout";
+import Top from "../../components/diner/top/Payment";
 
 const Payment = () => {
   return (
     <Layout>
-      <Container></Container>
+      <Container>
+        <div className="top">
+          <Top />
+        </div>
+      </Container>
     </Layout>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: auto;
+
+  .top {
+    width: 100%;
+    height: 120px;
+  }
+`;
 
 export default Payment;
