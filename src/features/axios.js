07a7ce35f8d7e603d9a0f342b57c8e92420460e6
Saad <byiringirosaad@gmail.com>
2022-05-18 16:14:39 +0200

    Update axios.js

--- src/features/axios.js
+++ src/features/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "https://mealpass-backend.herokuapp.com",
+  baseURL: "https://meals-back.herokuapp.com",
 });
