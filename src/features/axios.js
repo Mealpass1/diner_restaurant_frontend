bafbb55dbf83cac285f7d9d179301490cda2764c
Saad <byiringirosaad@gmail.com>
2022-05-23 20:21:30 +0200

    Update axios.js

--- src/features/axios.js
+++ src/features/axios.js
@@ -1,6 +1,3 @@
 import axios from "axios";
 
-export default axios.create({
-  // baseURL: "http://localhost:8080",
-  baseURL: "https://meals-back.herokuapp.com",
-});
+export default axios.create({});
